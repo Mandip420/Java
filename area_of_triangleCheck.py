ee5ce2a71c2c75b014f637fcd6dc1d497c606e07
Mandip Gupta <Gmandip57@gmail.com>
2024-03-11 07:36:29 +0545

    delete

--- area_of_triangleCheck.py
+++
@@ -1,7 +0,0 @@
-def area_of_triangle(length,breath):
-    area=length*breath
-    return area
-a=int(input("Enter the value of A\n"))
-b=int(input("Enter the value of B\n"))
-d=area_of_triangle(a,b)
-print("The area of Triangle = ",d)
